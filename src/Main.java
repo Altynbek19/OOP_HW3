8c2496b0e1e63c2335f1605c02b0720dda907b89
Altynbek19 <altynbek.zhonguchkaev@alatoo.edu.kg>
2024-10-21 04:00:02 +0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("isPrime(13): " + MathUtil.isPrime(13));
         System.out.println("gcd(15, 100): " + MathUtil.gcd(15, 100));
-        System.out.println("lcm(15, 100): " + MathUtil.lcm(15,100));
-        System.out.println("fibonacci(13): "+ MathUtil.fibonacci(13));
-        System.out.println("factorial(n): "+ MathUtil.factorial(13));
+        System.out.println("lcm(15, 100): " + MathUtil.lcm(15, 100));
+        System.out.println("fibonacci(13): " + MathUtil.fibonacci(13));
+        System.out.println("factorial(n): " + MathUtil.factorial(13));
 
         MathUtil mathUtil = new MathUtil();
         System.out.println("isPerfectNumber(28): " + mathUtil.isPerfectNumber(28));
